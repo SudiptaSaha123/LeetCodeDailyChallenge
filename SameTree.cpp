89135ad29fa62cdc9a898283294540f4f83aef20
“SudiptaSaha123” <sudiptasaha883@gmail.com>
2023-01-10 12:12:04 +0530

    SameTree.cpp file added

---
+++ SameTree.cpp
@@ -0,0 +1,17 @@
+// Problem Name : 100. Same Tree
+
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+
+        if(p == NULL || q == NULL){
+            return p == q;
+        }
+
+        if(p->val != q->val){
+            return false;
+        }
+
+        return isSameTree(p->left,q->left) && isSameTree(p->right,q->right);
+    }
+};
